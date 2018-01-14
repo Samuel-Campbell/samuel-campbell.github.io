2351f506ae4a30e79fc4a4bf000b49fcbc588753
samuel-campbell <samuel.pcampbell@gmail.com>
2018-01-14 10:11:01 -0500

    fixed all indents

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import TabNavigation from './Components/TabNavigation';
 import SkillBarChart from './Components/SkillBarChart';
 
 class App extends Component {
-  render() {
-    return (
-			<div className="App">				
-				<TabNavigation/>
-				<SkillBarChart/>
-     </div>	    
-    );
-  }
+    render() {
+        return (
+            <div className="App">				
+                <TabNavigation/>
+                <SkillBarChart/>
+            </div>	    
+        );
+    }
 }
 
 export default App;
--- src/Components/TabNavigation.js
+++ src/Components/TabNavigation.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import '../Theme/TabNavigation.css';
 
 class TabNavigation extends Component {
-  render() {
-		return (
-			<div id="tab-navigation-component">
-				<ul>
-					<li>Home</li>
-					<li>Skills</li>
-					<li>About</li>
-					<li>Contact</li>
-				</ul>  
-			</div>
-    );
-  }
+    render() {
+        return (
+            <div id="tab-navigation-component">
+                <ul>
+                    <li>Home</li>
+                    <li>Skills</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>  
+            </div>
+        );
+    }
 }
 
 export default TabNavigation;
