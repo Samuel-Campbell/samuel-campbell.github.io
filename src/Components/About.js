1714e559377ad56754faade9c7102cabe9e117e4
samuel-campbell <samuel.pcampbell@gmail.com>
2018-01-15 15:31:22 -0500

    fixed .vimrc file to fix indents

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,14 +4,14 @@ class About extends Component {
     render() {
         return (
             <div>
-				<h2>About</h2>
-				<div id="chatbot-reply">
-					Some reply message
-				</div>
-				<form>
-					<input type="text"/>
-					<input type="submit"/>
-				</form>
+                <h2>About</h2>
+                <div id="chatbot-reply">
+                    Some reply message
+                </div>
+                <form>
+                    <input type="text"/>
+                    <input type="submit"/>
+                </form>
             </div>
         );
     }
